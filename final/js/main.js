e6a43946cb83cc2cc0761ee3425f596aedeb421b
heinz143 <46978115+heinz143@users.noreply.github.com>
2019-12-21 22:59:51 -0600

    Add files via upload

---
+++ final/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+// all jquery and some javascript commands go here
+    var rellax = new Rellax('.rellax', {
+        speed: -2,
+        center: true,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });
+
+});
